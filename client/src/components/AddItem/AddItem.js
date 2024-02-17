10d6a2d8e824420d2e4af884efbd056c0e364ddb
Giorgia Dravelli <dravelligiorgia@gmail.com>
2024-02-17 14:27:17 +1100

    prova

--- client/src/components/AddItem/AddItem.js
+++ client/src/components/AddItem/AddItem.js
@@ -37,12 +37,6 @@ const AddItem = () => {
         setCategory("");
         setPrice("")
 
-        // axios
-        //     .post('http://localhost:3500/items', formData)
-        //     .then((res) => console.log(res.data))
-        //     .catch((err) => {
-        //         console.log(err.message)
-        //     })
         const result = await axios.post('http://localhost:3500/items', formData, { headers: { "Content-Type": "multipart/form-data"}})
         console.log(result)
         }
@@ -68,6 +62,7 @@ const AddItem = () => {
                             type="text"
                             name="title"
                             id="title"
+                            value={title}
                             placeholder="Title of the item"
                             onChange={(e) => setTitle(e.target.value)}
                         />
@@ -77,6 +72,7 @@ const AddItem = () => {
                             type="text"
                             name="description"
                             id="description"
+                            value={description}
                             placeholder="Description of the item"
                             onChange={(e) => setDescription(e.target.value)}
                         />
@@ -86,6 +82,7 @@ const AddItem = () => {
                             type="text"
                             name="category" 
                             id="category"
+                            value={category}
                             placeholder="Category of the item"
                             onChange={(e) => setCategory(e.target.value)}
                             >
@@ -96,6 +93,7 @@ const AddItem = () => {
                             type="number"
                             name="price"
                             id="price"
+                            value={price}
                             placeholder="Price of the item"
                             onChange={(e) => setPrice(e.target.value)}
                         />
@@ -121,105 +119,5 @@ const AddItem = () => {
     )
 
 }
-
-
-
-
-// class AddItem extends Component {
-//     state = {
-//         title: '',
-//         description: '',
-//         category: '',
-//         price: '',
-//     }
-
-//     static propTypes = {
-//         isAuthenticated: PropTypes.bool
-//     }
-
-//     onChange = (e) => {
-//         this.setState({[e.target.name]:e.target.value});
-//     }
-
-//     onSubmit = async (e) => {
-//         e.preventDefault();
-
-//         const newItem = {
-//             title: this.state.title,
-//             description: this.state.description,
-//             category: this.state.category,
-//             price: this.state.price
-//         }
-
-//         await this.props.addItem(newItem);
-
-//         // alert('Item added successfully');
-//     }
-
-//     render(){
-//         const token = localStorage.getItem('token')
-//         return(
-            // <div>
-            //     <Container>
-            //         <h2 className="text-center mb-3">Add a new Item</h2>
-            //         { token ?
-            //         <Form onSubmit={this.onSubmit}>
-            //             <FormGroup>
-            //                 <Label for="title">Title</Label>
-            //                 <Input
-            //                     type="text"
-            //                     name="title"
-            //                     id="title"
-            //                     placeholder="Title of the item"
-            //                     onChange={this.onChange}
-            //                 />
-            //                 <br/>
-            //                 <Label for="description">Description</Label>
-            //                 <Input
-            //                     type="text"
-            //                     name="description"
-            //                     id="description"
-            //                     placeholder="Description of the item"
-            //                     onChange={this.onChange}
-            //                 />
-            //                 <br/>
-            //                 <Label for="category">Category</Label>
-            //                 <Input 
-            //                     type="text"
-            //                     name="category" 
-            //                     id="category"
-            //                     placeholder="Category of the item"
-            //                     onChange={this.onChange}
-            //                     >
-            //                 </Input>
-            //                 <br/>
-            //                 <Label for="price">Price</Label>
-            //                 <Input
-            //                     type="number"
-            //                     name="price"
-            //                     id="price"
-            //                     placeholder="Price of the item"
-            //                     onChange={this.onChange}
-            //                 />
-                            
-            //                 <Button
-            //                     color="dark"
-            //                     style={{marginTop: '2rem'}}
-            //                     block
-            //                 >Add Item</Button>
-            //             </FormGroup>
-            //         </Form> : 
-            //         <Alert className="text-center" color="danger">Login to add items!</Alert>
-            //         }
-            //     </Container>
-            // </div>
-//         )
-//     }
-// }
-
-// const mapStateToProps = (state) => ({
-//     item: state.item,
-//     // isAuthenticated: state.auth.isAuthenticated,
-// });
 
 export default AddItem
